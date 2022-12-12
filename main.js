d19deb388bdeb4369cecce8e8199dbcdd8eeb534
Will Hedges <wshedges@outlook.com>
2022-12-12 09:39:56 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const quarterValue = (num) => {
+  const quarter = num / 4;
+  return quarter;
+};
+
+// Invoke the function and store the return value
+res = quarterValue(12);
+
+// Log the return value
+console.log(res);
